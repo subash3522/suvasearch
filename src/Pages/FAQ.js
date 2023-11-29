a1684268bf247e14a86711ddab6423a3ea2b40d9
subash <bhandarisubash22@gmail.com>
2023-11-29 21:29:46 +0545

    a

--- src/Pages/FAQ.js
+++ src/Pages/FAQ.js
@@ -3,16 +3,17 @@ import "../Global.css"; // Import your CSS file
 
 function App() {
   return (
-    <div className="faq p-3 container-sm" style={{border:'solid 2px black', borderRadius:'20px'}}>
+    <div
+      className="faq p-3 container-sm"
+      style={{ border: "solid 2px black", borderRadius: "20px" }}
+    >
       <header className="faq-header">
         <h1>Frequently Asked Questions</h1>
 
         <div className="faq-section">
           <h2 className="faq-section-title">Our Services FAQs</h2>
           <div className="faq-item">
-            <h3 className="faq-question">
-              Question: What do we offer?
-            </h3>
+            <h3 className="faq-question">Question: What do we offer?</h3>
             <p className="faq-answer">
               Answer: We offer different search helps
             </p>
@@ -26,12 +27,8 @@ function App() {
             </p>
           </div>
           <div className="faq-item">
-            <h3 className="faq-question">
-              Question: All Nepal Tour?
-            </h3>
-            <p className="faq-answer">
-              Answer: Happning Soon.
-            </p>
+            <h3 className="faq-question">Question: All Nepal Tour?</h3>
+            <p className="faq-answer">Answer: Happening Soon.</p>
           </div>
 
           {/* Add more FAQs */}
